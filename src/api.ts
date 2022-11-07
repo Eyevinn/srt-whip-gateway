fdd3de6848c55d1de58e343c4ce2a3457053f9b4
Jonas Birmé <jonas.birme@eyevinn.se>
2022-11-07 20:34:48 +0100

    feat: swagger api documentation

--- src/api.ts
+++ src/api.ts
@@ -1,4 +1,7 @@
 import Fastify from 'fastify';
+import cors from '@fastify/cors';
+import swagger from '@fastify/swagger';
+import swaggerUI from '@fastify/swagger-ui';
 import { TypeBoxTypeProvider } from '@fastify/type-provider-typebox';
 
 import ApiHealthcheck from './api/healthcheck';
@@ -12,6 +15,27 @@ export interface ApiOptions {
 
 export default (opts: ApiOptions) => {
   const api = Fastify({ ignoreTrailingSlash: true }).withTypeProvider<TypeBoxTypeProvider>();
+  api.register(cors, {});
+  api.register(swagger, {
+    swagger: {
+      info: {
+        title: 'SRT WHIP Gateway API',
+        description: 'API to manage SRT/WHIP transmitters',
+        version: 'v1'
+      },
+      securityDefinitions: {
+        apiKey: {
+          type: 'apiKey',
+          name: 'x-api-key',
+          in: 'header'
+        }
+      },
+    }
+  });
+  api.register(swaggerUI, {
+    routePrefix: '/api/docs'
+  });
+    
   api.register(ApiEngine, {
     prefix: '/api/v1',
     engine: opts.engine,
